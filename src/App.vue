224eec8ff15a9829414c98564b0871f446498757
burillu <burillu90@hotmail.it>
2023-11-15 12:27:52 +0100

    scaffolding with vite

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Pippo</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { store } from './data/store.js'
+export default {
+  name: 'App',
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
