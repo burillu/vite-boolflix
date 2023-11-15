5be5cb96ec1eb2cc5a866f938cc90b61e54f0ee4
burillu <burillu90@hotmail.it>
2023-11-15 15:19:51 +0100

    structure wrapper and tags

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <template>
-  <div>
-    <h1>Pippo <i class="fa-brands fa-sass fa-fade"></i></h1>
+  <div class="my-wrapper">
+    <header>
+      <AppHeader />
+    </header>
+    <main class="bg-secondary h-100">
+
+    </main>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import { store } from './data/store.js'
+import AppHeader from './components/AppHeader.vue';
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       store
-    }
+    };
   },
-  methods: {
-
-  },
+  methods: {},
   created() {
     // const url = this.store.apiUrl + this.store.endPoint.movie;
     // axios.get(url, { params: this.store.params }).then(resp => {
     //   console.log(resp)
     // })
-  }
+  },
+  components: { AppHeader }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.my-wrapper {
+  height: 100vh;
+}
+</style>
