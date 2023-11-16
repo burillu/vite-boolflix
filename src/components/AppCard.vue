9b7e09c9d92b3634c20e0b1998f39df3a71e6193
burillu <burillu90@hotmail.it>
2023-11-16 15:53:30 +0100

    add flip on hover cards

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,37 +1,43 @@
 <template>
     <div class="card-wrapper">
-        <div :id="id" v-if="!hoverCover" @mouseover="hoverCover = true" @mouseleave="hoverCover = false" class="h-100">
-            <img :src="src" :alt="name" class="rounded-2 my-img-card">
-            <!-- <div class="">
-                <div>titolo :{{ name }}</div>
-                <div>original title :{{ subtitle }}</div>
-                <div>original lang : <img class="my-flag" :src="srcFlag" :alt="data1">
+        <!-- <div :id="id" v-if="!hoverCover" @mouseover="hoverCover = true" @mouseleave="hoverCover = false" class="h-100"> -->
+        <div class="box">
+            <div class="box-inner">
+                <!-- cover -->
+                <div class="box-front">
+                    <div :id="id" class="h-100">
 
-                </div>
-                <div><i v-for="n in 5" class="fa-star" :class="(n < avgStar ? 'fa-solid text-warning' : 'fa-regular')" />
-                    <br>{{
-                        data2
-                    }}/10
-                </div>
-            </div> -->
-        </div>
-        <div v-else class="bg-black text-white p-2" id="overview" @mouseleave="hoverCover = false">
-            <div class="mb-3">
-                <div><span class="fw-bold">titolo:</span> {{ name }}</div>
-                <div><span class="fw-bold">original title:</span>{{ subtitle }}</div>
-                <div><span class="fw-bold">original lang:</span> <img class="my-flag" :src="srcFlag" :alt="data1">
+                        <img :src="src" :alt="name" class="rounded-2 my-img-card">
 
+
+                    </div>
                 </div>
-                <div> <span class="fw-bold">voto:</span><i v-for="n in 5" class="fa-star"
-                        :class="(n < avgStar ? 'fa-solid text-warning' : 'fa-regular')" /> <br>{{
-                            data2
-                        }}/10</div>
+                <!-- overview -->
+                <div class="box-back">
+                    <div class="bg-black text-white p-2 rounded-2" id="overview">
+                        <!-- <div v-else class="bg-black text-white p-2" id="overview" @mouseleave="hoverCover = false"> -->
+                        <div class="mb-3">
+                            <div><span class="fw-bold">titolo:</span> {{ name }}</div>
+                            <div><span class="fw-bold">original title:</span>{{ subtitle }}</div>
+                            <div><span class="fw-bold">original lang:</span> <img class="my-flag" :src="srcFlag"
+                                    :alt="data1">
+
+                            </div>
+                            <div> <span class="fw-bold">voto:</span><i v-for="n in 5" class="fa-star"
+                                    :class="(n < avgStar ? 'fa-solid text-warning' : 'fa-regular')" /> <br>{{
+                                        data2
+                                    }}/10</div>
+                        </div>
+                        <div class="">
+                            <p>{{ overview }}</p>
+                        </div>
+
+                    </div>
+                </div>
             </div>
-            <div class="">
-                <p>{{ overview }}</p>
-            </div>
-
         </div>
+
+
     </div>
 </template >
 
@@ -78,10 +84,53 @@ export default {
 
 .card-wrapper {
     height: 380px;
+    width: 100%;
 }
 
 #overview {
     overflow-y: auto;
     height: 100%;
+}
+
+// animation flip
+
+.box {
+    background-color: transparent;
+    width: 100%;
+    height: 100%;
+    //border: 1px solid #eeeeee;
+    perspective: 1000px;
+}
+
+.box-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.5s;
+    transform-style: preserve-3d;
+}
+
+.box:hover .box-inner {
+    transform: rotateY(180deg);
+}
+
+.box-front,
+.box-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+}
+
+.box-front {
+    //background-color: #cccccc;
+    color: #111111;
+}
+
+.box-back {
+    //background-color: #8ebf42;
+    color: #eeeeee;
+    transform: rotateY(180deg);
 }
 </style>
