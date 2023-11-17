a816cd1803b53c15528d6c2c9b7cdb8849e60443
burillu <burillu90@hotmail.it>
2023-11-17 23:58:30 +0100

    add App select comp

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,35 @@
+<template>
+    <select @change="selectToFiltre" class="form-select mx-3" aria-label="Default select example" v-model="store.selected">
+        <option value="">Scegli un genere</option>
+        <option v-for="item in store.genresList" :value="item.id">{{ item.name }}</option>
+
+    </select>
+</template>
+
+<script>
+import { store } from '../data/store.js'
+export default {
+    name: 'AppSelect',
+    data() {
+        return {
+
+            store
+        }
+    },
+    props: {
+
+    },
+    methods: {
+        selectToFiltre() {
+            if (!this.store.movieList) {
+                return
+            }
+
+
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped></style>
