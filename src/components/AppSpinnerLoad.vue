64b10a0a5dec4584cbbf35da13f1fd5ed04a36fc
burillu <burillu90@hotmail.it>
2023-11-17 19:14:46 +0100

    improve style spinnerLoad

---
+++ src/components/AppSpinnerLoad.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="spinner-container d-flex justify-content-center align-content-center">
+        <div class="spinner-border text-danger" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppSpinnerLoad'
+}
+</script>
+
+<style lang="scss" scoped>
+div.spinner-border {
+    width: 8em;
+    height: 8em;
+
+}
+
+.spinner-container {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 1000;
+}
+</style>
